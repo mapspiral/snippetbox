7c1dd58d0f30b7f11773bd7ba82536390ba67aa7
Robert van Drunen <r.j.m.vandrunen@gmail.com>
2021-12-07 21:11:24 +0100

    Added additional handlers

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 const ROOT = "/"
+const NOT_ALLOWED_STATUS_CODE = 405
 
 func home(writer http.ResponseWriter, request *http.Request) {
 	if request.URL.Path != ROOT {
@@ -16,9 +17,31 @@ func home(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte("Hello from Snippetbox"))
 }
 
+func showSnippet(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("showSnippet"))
+}
+
+func createSnippet(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		WriteNotAllowed(writer)
+		return
+	}
+
+	writer.Write([]byte("createSnippet"))
+}
+
+func WriteNotAllowed(writer http.ResponseWriter) {
+	writer.Header().Set("Allow", http.MethodPost)
+
+	writer.WriteHeader(NOT_ALLOWED_STATUS_CODE)
+	writer.Write([]byte("Method Not Allowed"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 	log.Println("Listening on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
